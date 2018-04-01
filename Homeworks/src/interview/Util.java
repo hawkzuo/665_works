52bb8c01c5fcceb0ff7f3df5d72419c926983e03
Jianyu Zuo <zuowings@gmail.com>
2018-04-01 14:52:02 -0500

    Add HW2 HW3

---
+++ Homeworks/src/interview/Util.java
@@ -0,0 +1,24 @@
+package interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Amos on 2018/2/9.
+ */
+public class Util {
+
+
+    public static List<int[]> readDataFromCSVFile(String fileName) {
+        List<int[]> result = new ArrayList<>();
+
+//        FileInputStream inputStream = new FileInputStream("");
+
+
+        return result;
+    }
+
+
+
+
+}
---
+++ Homeworks/submit/hw3/HW3Notes.rb
@@ -0,0 +1,30 @@
+--topo
+--mac
+--controller
+ip
+port = 6653 default
+ant is Java-based install via 'sudo apt-get install ant'
+
+FRESCO
+git clone https://github.com/xuraylei/fresco_floodlight.git
+
+
+intercept 3-way Handshake => Like TCP Proxy <= Apply only on TCP networks
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
